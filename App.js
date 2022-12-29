28344f33d60790f1948cf18a84b3829b1c343a41
gabriel natan <g.souza@grouplinknetwork.com>
2022-12-29 20:29:33 -0300

    create page home

--- App.js
+++ App.js
@@ -1,28 +1,10 @@
 import React from "react";
-import { SafeAreaView, ScrollView, StyleSheet, View } from "react-native";
-import { Card } from "./src/components/Card";
-import { Header } from "./src/components/Header"
-import { InputText } from "./src/components/InputText";
-import { MiniCards } from "./src/components/MiniCards";
-import comida1 from "./src/assets/comida-1.webp"
-import comida2 from "./src/assets/comida-2.jpg"
-import { Tabs } from "./src/components/Tabs";
+import { SafeAreaView, StyleSheet } from "react-native";
+import Home from "./src/pages/Home";
 export default function App(){
   return(
     <SafeAreaView style={styled.container}>
-      <Header/>
-      <InputText/>
-      <MiniCards/>
-      <ScrollView 
-        horizontal
-        showsHorizontalScrollIndicator={false}
-        showsVerticalScrollIndicator={false}
-        style={styled.conCard}
-      >
-        <Card img={comida1}/>
-        <Card img={comida2}/>
-      </ScrollView>
-      <Tabs/>
+      <Home/>
     </SafeAreaView>
   )
 }
@@ -32,9 +14,5 @@ const styled = StyleSheet.create({
     backgroundColor:"#FBF9F6",
     height:"100%",
     width:"100%"
-  },
-  conCard:{
-    flexDirection:"row",
-    padding:20
   }
 })
---
+++ src/pages/Home/index.js
@@ -0,0 +1,39 @@
+import React from "react";
+import { SafeAreaView, ScrollView, StyleSheet, View } from "react-native";
+import { Card } from "./src/components/Card";
+import { Header } from "./src/components/Header"
+import { InputText } from "./src/components/InputText";
+import { MiniCards } from "./src/components/MiniCards";
+import comida1 from "./src/assets/comida-1.webp"
+import comida2 from "./src/assets/comida-2.jpg"
+import { Tabs } from "./src/components/Tabs";
+export default function Home(){
+  return(
+    <View style={styled.container}>
+      <Header/>
+      <InputText/>
+      <MiniCards/>
+      <ScrollView 
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        showsVerticalScrollIndicator={false}
+        style={styled.conCard}
+      >
+        <Card img={comida1}/>
+        <Card img={comida2}/>
+      </ScrollView>
+      <Tabs/>
+    </View>
+  )
+}
+
+const styled = StyleSheet.create({
+  container:{
+    height:"100%",
+    width:"100%"
+  },
+  conCard:{
+    flexDirection:"row",
+    padding:20
+  }
+})
