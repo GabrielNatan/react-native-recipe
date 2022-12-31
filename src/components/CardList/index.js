a2e50ccd1c439083be2ba1f631c56b7e107ba0ea
gabriel natan <g.souza@grouplinknetwork.com>
2022-12-31 14:25:04 -0300

    style cardList

--- src/components/CardList/index.js
+++ src/components/CardList/index.js
@@ -1,8 +1,10 @@
-import { Image, StyleSheet, Text, Touchable, View } from "react-native"
-
+import { Image, StyleSheet, Text, TouchableOpacity, View } from "react-native"
+import hamburguer from "../../assets/hamburguer.jpg"
+import Icons from "react-native-vector-icons/FontAwesome5"
+import Icon from "react-native-vector-icons/Octicons"
 export const CardList = ()=>{
     return(
-        <Touchable style={styled.contCard}>
+        <TouchableOpacity style={styled.contCard}>
             <View style={styled.contImage}>
                 <Image  style={styled.image} source={hamburguer}/>
             </View>
@@ -11,27 +13,62 @@ export const CardList = ()=>{
                 <Text  style={styled.subTitle}>6 ingredients</Text>
                 <View  style={styled.cont}>
                     <View  style={styled.info}>
+                        <Icons name="clock" size={15} color="orange"/>
                         <Text  style={styled.subTitle}>30 min</Text>
                     </View>
                     <View  style={styled.info}>
+                        <Icons name="fire" size={15} color="orange"/>
                         <Text  style={styled.subTitle}>30 min</Text>
                     </View>
                     <View  style={styled.info}>
+                        <Icon name="people" size={15} color="orange"/>
                         <Text  style={styled.subTitle}>30 min</Text>
                     </View>
                 </View>
             </View>
-        </Touchable>
+        </TouchableOpacity>
     )
 }
 
-const Styled = StyleSheet.create({
-    contCard:{},
-    contImage:{},
-    image:{},
-    contTitle:{},
-    title:{},
-    subTitle:{},
-    cont:{},
-    info:{},
+const styled = StyleSheet.create({
+    contCard:{
+        padding:10,
+        flexDirection:"row",
+        backgroundColor:"#FFF",
+        borderBottomWidth:1,
+        borderBottomColor:"#CCC",
+        alignItems:"center"
+    },
+    contImage:{
+        width:90,
+        height:90,
+        overflow:"hidden",
+        borderRadius:10
+    },
+    image:{
+        width:"100%",
+        height:"100%"
+    },
+    contTitle:{
+        padding:10
+    },
+    title:{
+        fontSize:18,
+        fontWeight:"bold"
+    },
+    subTitle:{
+        fontSize:12,
+        color:"#999"
+    },
+    cont:{
+        width:"75%",
+        flexDirection:"row",
+        justifyContent:"space-between"
+    },
+    info:{
+        width:"25%",
+        paddingTop:10,
+        flexDirection:"row",
+        justifyContent:"space-between"
+    },
 })
