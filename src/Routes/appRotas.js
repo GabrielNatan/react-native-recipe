fa11143e62a07847b4427cbc4125bd7d549524a4
gabriel natan <g.souza@grouplinknetwork.com>
2023-01-01 20:20:58 -0300

    verify login

--- src/Routes/appRotas.js
+++ src/Routes/appRotas.js
@@ -1,15 +1,22 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 
 import RoutesHome from './RoutesHome';
 import { Login } from '../pages/Login';
 import { CreateAccount } from '../pages/CreateAccount';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { useNavigation } from '@react-navigation/native';
 const Stack = createNativeStackNavigator();
 
 
 
 export default function AppRotas() {
+  const { navigate } = useNavigation()
+  useEffect(async()=>{
+    let islogged = await AsyncStorage.getItem("login")
+    if(islogged){navigate("Home")}
+  },[])
   return <Stack.Navigator screenOptions={{headerShown:false}}>
       <Stack.Screen name='Create account' component={CreateAccount} />
       <Stack.Screen name='Login' component={Login} />
